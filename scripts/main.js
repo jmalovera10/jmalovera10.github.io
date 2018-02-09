f813aa87fa6102f49ee7cd1fb8c9f5f021ffb180
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-09 08:43:28 -0500

    Titles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 function main(){
-    //$('#name-header').hide();
-    //$('#name-header').fadeIn(1000);
+    'use strict';
+
+    var c, currentScrollTop = 0,
+        navbar = $('nav');
+
+    $(window).scroll(function () {
+        var a = $(window).scrollTop();
+        var b = navbar.height();
+
+        currentScrollTop = a;
+
+        if (c < currentScrollTop && a > b + b) {
+            navbar.addClass("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.removeClass("scrollUp");
+        }
+        c = currentScrollTop;
+    });
 }
 $(document).ready(main);

1c78cc21690a197ad36116888e3fc03122f2acf0
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-09 09:34:09 -0500

    Scrolldown cancel

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,5 @@
 function main(){
     'use strict';
 
-    var c, currentScrollTop = 0,
-        navbar = $('nav');
-
-    $(window).scroll(function () {
-        var a = $(window).scrollTop();
-        var b = navbar.height();
-
-        currentScrollTop = a;
-
-        if (c < currentScrollTop && a > b + b) {
-            navbar.addClass("scrollUp");
-        } else if (c > currentScrollTop && !(a <= b)) {
-            navbar.removeClass("scrollUp");
-        }
-        c = currentScrollTop;
-    });
 }
 $(document).ready(main);

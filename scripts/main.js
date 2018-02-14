5cf24b5d7a7f5e4710811fdc0df6d52aa8e1469e
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-14 16:30:19 -0500

    Hobbies complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /* global $ */
 
-function main(){
+function main() {
     'use strict';
     /*$(window).resize(function(){
         var tamPagina = $(document).height();
@@ -12,30 +12,57 @@ function main(){
     /*----------------------------------------
     Skill set initialization inside index.html
      -----------------------------------------*/
-    function callback(data){
+    function callbackSkills(data) {
         var i = 1;
-        data.forEach(function(d){
+        data.forEach(function (d) {
             $("#stats").append($("<div class='col-sm-8'>"))
                 .append($("<div class='row justify-content-around'>")
-                    .append($("<div class='col-sm-4'>")
-                        .append($("<img class='skill-image'>")
-                            .attr("src",d.image_path)
-                            .attr("alt",d.name)))
-                    .append($("<div class='col-sm-8'>")
+                    .append($("<div class='col-sm-3 center-items'>")
+                        .append($("<img class='skill-image img-responsive'>")
+                            .attr("src", d.image_path)
+                            .attr("alt", d.name)))
+                    .append($("<div class='col-sm-9 center-items'>")
                         .append($("<div id='my-progress'>")
-                            .append($("<div id='skill"+i+"'>")
+                            .append($("<div id='skill" + i + "'>")
                                 .text("10%"))))
-            );
+                );
             //if($('#skill'+i).visible(true)){
-                move(d.value,"skill"+i);
+            move(d.value, "skill" + i);
             //};
             i++;
         })
 
     }
 
-    //Data upload
-    $.getJSON("data/skills.json",callback);
+    /*----------------------------------------
+    Hobbies set initialization inside index.html
+     -----------------------------------------*/
+    function callbackHobbies(data) {
+        data.forEach(function (d) {
+            $("#hobbies").append($("<div class='col-sm-4 center-items'>")
+                .append($("<div class='row justify-content-around center-items wow flipInX'>")
+                    .append($("<div class='col center-items'>")
+                        .append($("<img class='hobbies-image img-responsive'>")
+                            .attr("src", d.img_src)
+                            .attr("alt", d.name))))
+                .append($("<div class='row justify-content-around center-items wow rubberBand'>")
+                    .append($("<div class='col center-items'>")
+                        .append($("<h2 class='hobbies-description'>")
+                            .text(d.name))))
+            )
+        })
+    };
+
+
+    /*----------------------------------------
+    Hobbies set initialization inside index.html
+     -----------------------------------------*/
+
+//Skills data upload
+    $.getJSON("data/skills.json", callbackSkills);
+
+//Hobbies data upload
+    $.getJSON("data/hobbies.json", callbackHobbies);
 
     /*----------------------
     Dynamic barfill function
@@ -44,6 +71,7 @@ function main(){
         var elem = document.getElementById(skill);
         var width = 10;
         var id = setInterval(frame, 20);
+
         function frame() {
             if (width >= value) {
                 clearInterval(id);
@@ -55,4 +83,5 @@ function main(){
         }
     }
 }
+
 $(document).ready(main);

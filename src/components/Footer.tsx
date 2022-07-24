47a000fee97cff154d088d984ee43128d82dd854
jmalovera10 <jma.lovera10@uniandes.edu.co>
2022-07-24 10:35:12 -0500

    Fix prettier and eslint configuration and add airbnb rules

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className='flex flex-row'>
-      <div className='flex-1'>
+    <div className="flex flex-row">
+      <div className="flex-1">
         <h1>Hello world!</h1>
       </div>
-      <div className='flex-1'>
+      <div className="flex-1">
         <h1>Hello from the other side!</h1>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#011627",
+      secondary: "#FF3366",
+      success: "#2EC4B6",
+      warning: "#F2E863",
+    },
+    fontFamily: {
+      sans: ["Nobile", "sans-serif"],
+      serif: ["Source", "serif"],
+    },
   },
   plugins: [],
 };

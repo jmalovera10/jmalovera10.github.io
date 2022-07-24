1ba4ddd1ddbe6ae8c09d2ef73e7b4b4f2ecaf253
jmalovera10 <jma.lovera10@uniandes.edu.co>
2022-07-24 09:41:38 -0500

    Setup react project for v2

---
+++ my-app/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className='flex flex-row'>
+      <div className='flex-1'>
+        <h1>Hello world!</h1>
+      </div>
+      <div className='flex-1'>
+        <h1>Hello from the other side!</h1>
+      </div>
+    </div>
+  );
+}

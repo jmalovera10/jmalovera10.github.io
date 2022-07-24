6f5716262d4785a915972623560fa5314a93fa7f
jmalovera10 <jma.lovera10@uniandes.edu.co>
2022-07-24 11:01:58 -0500

    Initial configurations for linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Footer />
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Footer() {
+export default function Navbar() {
   return (
     <div className="flex flex-row">
       <div className="flex-1">
         <h1>Hello world!</h1>
       </div>
       <div className="flex-1">
-        <h1>Hello from the other side!</h1>
+        <h2>Hello from the other side!</h2>
       </div>
     </div>
   );

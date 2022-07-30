ffe377a3f38aa217f116af8135ca4b49f36e6ff8
jmalovera10 <jma.lovera10@uniandes.edu.co>
2022-07-30 11:59:18 -0500

    Add page sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Education />
+      <Skills />
+      <Footer />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
     colors: {
       primary: "#011627",
       secondary: "#FF3366",
       success: "#2EC4B6",
-      warning: "#F2E863",
+      warning: "#F2E863"
     },
     fontFamily: {
       sans: ["Nobile", "sans-serif"],
-      serif: ["Source", "serif"],
-    },
+      serif: ["Source", "serif"]
+    }
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")]
 };
